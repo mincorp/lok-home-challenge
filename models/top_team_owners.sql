ea010d4d4ddbf31e58405d9e92369ae0dc8ea828
mintzaspan@gmail.com <mintzaspan@gmail.com>
2022-09-25 23:18:20 +0000

    get owners list for top customers

---
+++ models/top_team_owners.sql
@@ -0,0 +1,18 @@
+with
+
+owners as (
+    SELECT
+        user_id
+       ,team_id
+
+     FROM `lok-home-challenge.lok.users`
+
+     where invited_by is null
+)
+
+select *
+
+from owners
+
+inner join top_customers
+on owners.team_id = top_customers.team_id
